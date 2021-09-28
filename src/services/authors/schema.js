f88db7a9f677d3f57450294d91fbd9f1582fceb8
hasham zafar <hasham307@gmail.com>
2021-09-28 16:35:53 +0200

    module-8-
    module 8 day 2 homework

--- src/services/authors/schema.js
+++ src/services/authors/schema.js
@@ -2,12 +2,47 @@ import mongoose from "mongoose"
 
 const { Schema, model } = mongoose
 
-const AuthorSchema = new Schema(
+const authorSchema = new Schema(
     {
         name: { type: String, required: true },
         surname: { type: String, required: true },
+        password: { type: String, required: true },
+        email: { type: String, required: true },
     },
     { timestamps: true }
 )
+authorSchema.pre("save", async function (next) {
+    const newAuthor = this
+    const plainPw = newUser.password
+    if (newAuthor.isModified("password")) {
+        newAuthor.password = await bcrypt.hash(plainPw, 10)
+    }
+    next()
+})
 
-export default model("Author", AuthorSchema)
+authorSchema.methods.toJson = function () {
+    const authorDocument = this
+    const authorObject = authorDocument.toObject()
+
+    delete authorObject.password
+
+    return authorObject
+
+}
+
+authorSchema.statics.checkCredentials = async function (email, plainPw) {
+    const author = this.findOne({ email })
+
+    if (author) {
+        const isMatch = await bcrypt.compare(plainPw, author.password)
+        if (isMatch) return author
+        else return null
+    } else {
+        return null
+    }
+
+}
+
+
+
+export default model("Author", authorSchema)
